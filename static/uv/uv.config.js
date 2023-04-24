5c73850a34161d81edc3fa8bce74c17016533d7a
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-04-24 18:23:56 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/uv/',
+    prefix: '/uvservice/',
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
